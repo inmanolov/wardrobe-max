85ce480a1e4bc9874a8a40828b3f3ea916a4c34e
inmanolov <ivan.nadkov@gmail.com>
2023-02-12 19:53:23 +0200

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 
 const Navbar = () => {
     const { data: session, status } = useSession();
+    
 
     const logOutClickHandler = () => {
         signOut({ callbackUrl: '/' });
